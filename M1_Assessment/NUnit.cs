98f98891b62f5ee1bb704e0839bfb5fd9511490c
varshith3555 <varshithreddy661@gmail.com>
2026-02-02 15:08:45 +0530

    new commit

--- M1_Assessment/NUnit.cs
+++ M1_Assessment/NUnit.cs
@@ -8,59 +8,39 @@ public class BankAccountTests
     [SetUp]
     public void Setup()
     {
-        // Initialize bank account with initial balance of 1000
         bankAccount = new Program(1000m);
     }
 
-    // Test Deposit method with valid amount
     [Test]
     public void Test_Deposit_ValidAmount()
     {
-        // Arrange
         decimal depositAmount = 500m;
         decimal expectedBalance = 1500m;
 
-        // Act
         bankAccount.Deposit(depositAmount);
-
-        // Assert
         Assert.AreEqual(expectedBalance, bankAccount.Balance);
     }
 
-    // Test Deposit method with negative amount
     [Test]
     public void Test_Deposit_NegativeAmount()
     {
-        // Arrange
         decimal negativeAmount = -100m;
-
-        // Act & Assert
         Assert.Throws<System.InvalidOperationException>(() => bankAccount.Deposit(negativeAmount));
     }
 
-    // Test Withdraw method with valid amount
     [Test]
     public void Test_Withdraw_ValidAmount()
     {
-        // Arrange
         decimal withdrawAmount = 300m;
         decimal expectedBalance = 700m;
-
-        // Act
         bankAccount.Withdraw(withdrawAmount);
-
-        // Assert
         Assert.AreEqual(expectedBalance, bankAccount.Balance);
     }
 
-    // Test Withdraw method with insufficient funds
-    [Test]
+    [Test]GCNotificationStatus
     public void Test_Withdraw_InsufficientFunds()
     {
-        // Arrange
         decimal withdrawAmount = 1500m;
-
-        // Act & Assert
         Assert.Throws<System.InvalidOperationException>(() => bankAccount.Withdraw(withdrawAmount));
     }
 }
